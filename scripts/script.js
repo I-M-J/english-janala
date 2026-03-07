eba189ae2bcc1e387b2f82584939e84cdcc4bf8d
I-M-J <149041998+I-M-J@users.noreply.github.com>
2026-03-07 12:03:08 +0600

    Showing model functionality added with styling

--- scripts/script.js
+++ scripts/script.js
@@ -31,12 +31,12 @@ const loadLevelWords = (levelNo) => {
     const url = `https://openapi.programming-hero.com/api/level/${levelNo}`;
 
     fetch(url).then((response) => response.json()).then((json) => {
-                                                                displayLevelWords(json);
+        displayLevelWords(json);
 
-                                                                [...document.getElementsByClassName('btn-lesson')].forEach((btn) => btn.classList.remove('active'));
+        [...document.getElementsByClassName('btn-lesson')].forEach((btn) => btn.classList.remove('active'));
 
-                                                                document.getElementById(`btn-lesson-${levelNo}`).classList.add('active');
-                                                            });
+        document.getElementById(`btn-lesson-${levelNo}`).classList.add('active');
+    });
 }
 
 const displayLevelWords = (json) => {
@@ -60,7 +60,7 @@ const displayLevelWords = (json) => {
                     <h2 class="hind-siliguri-font font-medium text-4xl leading-10 text-[#292524]">নেক্সট Lesson এ যান</h2>
             </div>
         `
-        
+
         return;
     }
 
@@ -81,13 +81,70 @@ const displayLevelWords = (json) => {
             </div>
 
             <div class="flex justify-between">
-                <button class="btn h-14 w-14 bg-[#1A91FF]/10 text-2xl rounded-lg border-none"><i
+                <button onclick="loadWordDetails(${word.id})" class="btn h-14 w-14 bg-[#1A91FF]/10 text-2xl rounded-lg border-none"><i
                         class="fa-solid fa-circle-info"></i></button>
 
                 <button class="btn h-14 w-14 bg-[#1A91FF]/10 text-2xl rounded-lg border-none"><i
                         class="fa-solid fa-volume-high"></i></button>
             </div>
         `
+
         wordContainer.append(wordCard);
     });
+}
+
+const loadWordDetails = async (wordId) => {
+    const url = `https://openapi.programming-hero.com/api/word/${wordId}`;
+
+    const response = await fetch(url);
+
+    const json = await response.json();
+
+    displayWordDetails(json);
+}
+
+const displayWordDetails = (json) => {
+    const wordDetails = document.getElementById('word-details');
+    wordDetails.innerHTML = "";
+
+    wordDetails.className = "rounded-xl border-2 border-[#EDF7FF] p-6 space-y-8";
+
+    wordDetails.innerHTML = `
+        <h2 class="font-semibold text-4xl leading-10">${json.data.word} (<i class="fa-solid fa-microphone-lines"></i>:${json.data.pronunciation})</h2>
+
+        <div>
+            <h3 class="font-semibold text-2xl leading-10 mb-2.5">Meaning</h3>
+
+            <p class="hind-siliguri-font font-medium text-2xl leading-10">${json.data.meaning}</p>
+        </div>
+
+        <div>
+            <h3 class="font-semibold text-2xl leading-10 mb-2">Example</h3>
+
+            <p class="font-normal text-2xl leading-10">${json.data.sentence}</p>
+        </div>
+
+        <div>
+            <h3 class="hind-siliguri-font font-medium text-2xl leading-10 mb-2.5">সমার্থক শব্দ গুলো</h3>
+
+            <div id="word-details-syn" class="flex gap-4.5 flex-wrap">
+                
+            </div>
+        </div>
+    `
+
+    const wordDetailsSyn = document.getElementById('word-details-syn');
+    wordDetailsSyn.innerHTML = '';
+
+    json.data.synonyms.forEach((word) => {
+        const synWord = document.createElement('div');
+
+        synWord.className = 'inline-block py-1.5 px-5 font-normal text-xl leading-10 bg-[#EDF7FF] border-1 border-[#D7E4EF] rounded-md';
+
+        synWord.innerHTML = `${word}`;
+
+        wordDetailsSyn.append(synWord);
+    })
+
+    document.getElementById('word-modal').showModal();
 }
